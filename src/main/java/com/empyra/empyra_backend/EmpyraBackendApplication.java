ad55770afbd60545d9cf2e9526876a5597263492
essinaya <eternal.fantasy03@gmail.com>
2025-08-17 16:38:24 +0800

    Spring initializing

---
+++ src/main/java/com/empyra/empyra_backend/EmpyraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.empyra.empyra_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpyraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpyraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empyra/empyra_backend/EmpyraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empyra.empyra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpyraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
